15c9d41897d39c60562addc0e819d6e2f4c8d4bf
xvnian <xvnian@163.com>
2018-01-10 16:50:49 +0800

    util vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'normalize.css'
+import 'animate.css'
 
 import Vue from 'vue'
 import App from './App'
@@ -18,7 +19,7 @@ axios.interceptors.response.use(response => {
 })
 
 Vue.use(VueLazyload, {
-  error : '',
+  error: '',
   loading: '',
   attempt: 1
 })
@@ -30,5 +31,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
